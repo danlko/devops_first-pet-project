587ef91a89d66ba9792123057b8bbcc342b1d32b
danlko <d.rachkev@gmail.com>
2026-01-24 17:06:03 +0200

    Health Checking sctipts were added v3

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ def hello_devops():
     cursor.close()
     connection.close()
 
-    return jsonify("HELLO MY LITTLE GIT!")
+    return jsonify("HELLO MIE LITTLE GIT!")
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=5000)
--- tests/health_check.sh
+++ tests/health_check.sh
@@ -4,7 +4,7 @@ URL=${1:-"http://3.239.116.53:5000/"}
 
 echo "Health checking..."
 
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code" "$URL")
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
 
 if [ "$STATUS_CODE" -eq 200 ]; then
   echo "SUCCESS! status code is $STATUS_CODE"
