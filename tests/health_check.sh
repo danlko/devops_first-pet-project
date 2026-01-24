97df23d6282002c9ebb1e50bab4a4e2ba5ed4388
danlko <d.rachkev@gmail.com>
2026-01-24 16:55:51 +0200

    Health Checking sctipts were added

---
+++ tests/health_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+URL=${1:-"http://3.239.116.53:5000/"}
+
+echo "Health checking..."
+
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code" "$URL")
+
+if [ "$STATUS_CODE" -eq 200 ]; then
+  echo "SUCCESS! status code is $STATUS_CODE"
+  exit 0
+else
+  echo "FAILED! status code is $STATUS_CODE"
+  exit 1
+fi
+
+
